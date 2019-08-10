8e40d57b45cf2307eec306e615dc46903179c327
Patryk <desantowski@gmail.com>
2019-08-10 10:58:20 +0200

    Revert "Revert "Multithreading does work, server properly detects clients disconnect""
    This reverts commit 75ca0ed5dd87d462e40d12917cc9f3dc90fcfa25.

--- Server/TCPClientService.cpp
+++ Server/TCPClientService.cpp
@@ -1,7 +1,17 @@
 #include "TCPClientService.h"
+#include <iostream>
 
-TCPClientService::TCPClientService(SOCKET& socket) : winSocket{socket} { }
+TCPClientService::TCPClientService(std::shared_ptr<SOCKET>& socket) 
+	: winSocketPtr(socket) {
+}
 
 void TCPClientService::handleConnection() {
+	std::cout << "Handled connection from socket...\n";
+	char recvbuf[5] = "xdxd";
+	int bytesRecv = send(*winSocketPtr, recvbuf, 4, 0);
 
+	while (!(bytesRecv == -1 || bytesRecv == WSAECONNRESET) ) {
+		bytesRecv = send(*winSocketPtr, recvbuf, 4, 0);
+	}
+	std::cout << "Connection closed\n";
 }
