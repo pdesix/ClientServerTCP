75ca0ed5dd87d462e40d12917cc9f3dc90fcfa25
Patryk <desantowski@gmail.com>
2019-08-10 10:58:14 +0200

    Revert "Multithreading does work, server properly detects clients disconnect"
    This reverts commit a70fa4f12c8ccf69a7407ef35047214434803789.

--- Server/TCPClientService.cpp
+++ Server/TCPClientService.cpp
@@ -1,17 +1,7 @@
 #include "TCPClientService.h"
-#include <iostream>
 
-TCPClientService::TCPClientService(std::shared_ptr<SOCKET>& socket) 
-	: winSocketPtr(socket) {
-}
+TCPClientService::TCPClientService(SOCKET& socket) : winSocket{socket} { }
 
 void TCPClientService::handleConnection() {
-	std::cout << "Handled connection from socket...\n";
-	char recvbuf[5] = "xdxd";
-	int bytesRecv = send(*winSocketPtr, recvbuf, 4, 0);
 
-	while (!(bytesRecv == -1 || bytesRecv == WSAECONNRESET) ) {
-		bytesRecv = send(*winSocketPtr, recvbuf, 4, 0);
-	}
-	std::cout << "Connection closed\n";
 }
--- Server/TCPClientService.h
+++ Server/TCPClientService.h
@@ -1,15 +1,14 @@
 #pragma once
 #include <winsock2.h>
 #include <Ws2tcpip.h>
-#include <memory>
 #include "IClientService.h"
 
 class TCPClientService : public IClientService {
 public:
-	TCPClientService(std::shared_ptr<SOCKET>& socket);
+	TCPClientService(SOCKET& socket);
 	virtual void handleConnection() override;
 
 
 private:
-	std::shared_ptr<SOCKET> winSocketPtr;
+	SOCKET& winSocket;;
 };
